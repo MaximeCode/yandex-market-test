f0b67fb736ec0fb9ff0896290a0c95b55ebc54a5
maxime <maxime.mose>
2020-10-27 23:09:56 +0300

    fix

--- src/main/java/pages/MarketSectionPage.java
+++ src/main/java/pages/MarketSectionPage.java
@@ -24,7 +24,7 @@ public class MarketSectionPage {
             menuItems.stream()
                     .filter(e -> e.getText().equals(item))
                     .findFirst()
-                    .orElseThrow(() -> new Exception("Не найден пункт меню."))
+                    .orElseThrow(() -> new Exception("Не найден пункт меню \"" + item +"\"."))
                     .click();
         } catch (Exception e) {
             e.printStackTrace();
