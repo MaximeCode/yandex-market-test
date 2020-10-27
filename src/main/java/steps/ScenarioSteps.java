c634cb6d91547359de0203ce1e3fb6e52ff99c63
maxime <maxime.mose>
2020-10-27 23:16:07 +0300

    fix

--- src/main/java/steps/ScenarioSteps.java
+++ src/main/java/steps/ScenarioSteps.java
@@ -33,42 +33,42 @@ public class ScenarioSteps {
         marketSectionPageSteps.clickFiltersButton();
     }
 
-    @Допустим("Задаём параметр поиска по цене от {string} рублей.")
+    @Допустим("Задаём параметр поиска по цене от {string} рублей")
     public void задаём_параметр_поиска_по_цене_от(String string) {
         filtersPageSteps.inputPriceFrom(string);
     }
 
-    @Допустим("Выбираем производителей {string} и {string}.")
+    @Допустим("Выбираем производителей {string} и {string}")
     public void выбираем_производителей_(String... strings) {
         filtersPageSteps.clickMakerCheckboxes(strings);
     }
 
-    @Допустим("Выбираем производителей {string}.")
+    @Допустим("Выбираем производителей {string}")
     public void выбираем_производителей_(String string) {
         filtersPageSteps.clickMakerCheckboxes(string);
     }
 
-    @Допустим("Нажимаем кнопку \"Применить\".")
+    @Допустим("Нажимаем кнопку \"Применить\"")
     public void нажимаем_кнопку_Применить() {
         filtersPageSteps.clickShowButton();
     }
 
-    @Допустим("Проверяем, что элементов на странице {int}.")
+    @Допустим("Проверяем, что элементов на странице {int}")
     public void проверяем_что_элементов_на_странице(Integer number) {
         itemsPageSteps.checkNumberOfItemsOnPage(number);
     }
 
-    @Допустим("Запоминаем первый элемент в списке.")
+    @Допустим("Запоминаем первый элемент в списке")
     public void запоминаем_первый_элемент_в_списке() {
         stash = itemsPageSteps.getItemTitleByNumber(1);
     }
 
-    @Допустим("В поисковую строку вводим запомненное значение.")
+    @Допустим("В поисковую строку вводим запомненное значение")
     public void в_поисковую_строку_вводим_запомненное_значение() {
         itemsPageSteps.search(stash);
     }
 
-    @Допустим("Находим и проверяем, что наименование товара соответствует запомненному значению.")
+    @Допустим("Находим и проверяем, что наименование товара соответствует запомненному значению")
     public void находим_и_проверяем_что_наименование_товара_соответствует_запомненному_значению() {
         itemsPageSteps.checkItemTitleByNumber(1, stash);
     }
