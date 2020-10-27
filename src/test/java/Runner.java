84f5d25e93d5ef3a915aadbd7d240b6708978a9c
maxime <maxime.mose>
2020-10-28 00:29:38 +0300

    fix

--- src/test/java/Runner.java
+++ src/test/java/Runner.java
@@ -3,8 +3,8 @@ import io.cucumber.junit.CucumberOptions;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features = "src/test/resources", glue = "steps"
-//        plugin = ""
+@CucumberOptions(features = "src/test/resources", glue = "steps",
+        plugin = "io.qameta.allure.cucumber6jvm.AllureCucumber6Jvm"
 )
 public class Runner {
 }
